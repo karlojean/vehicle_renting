c2ddc6a942451417c4d6133b17087f2f63a602c5
Jean Karlo <jeankarlodev@gmail.com>
2025-11-25 22:04:29 -0300

    feat: init project

---
+++ src/main/java/dev/jeankarlo/vehiclerenting/VehicleRentingApplication.java
@@ -0,0 +1,13 @@
+package dev.jeankarlo.vehiclerenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentingApplication.class, args);
+    }
+
+}
