cd73803e884a262e7962d9da75da69f09ddffdee
Jean Karlo <jeankarlodev@gmail.com>
2025-12-29 22:01:50 -0300

    feat: add email normalization in AccountMapper

--- src/main/java/dev/jeankarlo/vehiclerenting/mapper/AccountMapper.java
+++ src/main/java/dev/jeankarlo/vehiclerenting/mapper/AccountMapper.java
@@ -5,6 +5,7 @@ import dev.jeankarlo.vehiclerenting.dto.account.AccountResponseDTO;
 import dev.jeankarlo.vehiclerenting.entity.Account;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
@@ -12,7 +13,15 @@ public interface AccountMapper {
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "authorities", ignore = true)
+    @Mapping(source = "email", target = "email", qualifiedByName = "normalizeEmail")
     Account toEntity(AccountCreateDTO dto);
 
     AccountResponseDTO toResponseDTO(Account account);
+
+    @Named("normalizeEmail")
+    default String normalizeEmail(String email) {
+        if (email == null)
+            return null;
+        return email.trim().toLowerCase();
+    }
 }
