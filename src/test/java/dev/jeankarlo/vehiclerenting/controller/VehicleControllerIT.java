ef2152a62d82b17cf833146f14a17e5d0f33fdd9
Jean Karlo <jeankarlodev@gmail.com>
2026-01-03 21:14:16 -0300

    feat: add integration test in vehicle controller

--- src/test/java/dev/jeankarlo/vehiclerenting/controller/VehicleControllerIT.java
+++ src/test/java/dev/jeankarlo/vehiclerenting/controller/VehicleControllerIT.java
@@ -4,30 +4,33 @@ package dev.jeankarlo.vehiclerenting.controller;
 import dev.jeankarlo.vehiclerenting.BaseAuthenticatedTest;
 import dev.jeankarlo.vehiclerenting.dto.vehicle.VehicleRequestDTO;
 import dev.jeankarlo.vehiclerenting.entity.Account;
+import dev.jeankarlo.vehiclerenting.entity.Booking;
 import dev.jeankarlo.vehiclerenting.entity.Location;
 import dev.jeankarlo.vehiclerenting.entity.Vehicle;
+import dev.jeankarlo.vehiclerenting.entity.enums.BookingStatus;
 import dev.jeankarlo.vehiclerenting.entity.enums.VehicleFuelType;
 import dev.jeankarlo.vehiclerenting.entity.enums.VehicleType;
 import dev.jeankarlo.vehiclerenting.repository.AccountRepository;
+import dev.jeankarlo.vehiclerenting.repository.BookingRepository;
 import dev.jeankarlo.vehiclerenting.repository.LocationRepository;
 import dev.jeankarlo.vehiclerenting.repository.VehicleRepository;
 import io.restassured.http.ContentType;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Random;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-@Slf4j
 public class VehicleControllerIT extends BaseAuthenticatedTest {
 
     @Autowired
@@ -40,9 +43,12 @@ public class VehicleControllerIT extends BaseAuthenticatedTest {
     private LocationRepository locationRepository;
     @Autowired
     private VehicleRepository vehicleRepository;
+    @Autowired
+    private BookingRepository bookingRepository;
 
     @BeforeEach
     void cleanUp() {
+        bookingRepository.deleteAll();
         vehicleRepository.deleteAll();
         locationRepository.deleteAll();
         accountRepository.deleteAll();
@@ -229,6 +235,164 @@ public class VehicleControllerIT extends BaseAuthenticatedTest {
             .get("/vehicles/{id}", foreignPartnerVehicle.getId())
         .then()
             .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Should delete vehicle by id successfully")
+    void shouldDeleteVehicleByIdSuccessfully() {
+        String token = createAndLoginAsRentingPartner();
+        Account account = getCurrentAccount(token);
+        Location location = createAndSaveLocation(account);
+        Vehicle vehicle = createVehicle(location, account);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+        .when()
+            .delete("/vehicles/{id}", vehicle.getId())
+        .then()
+            .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("Should not delete vehicle by id when it belongs to another partner")
+    void shouldNotDeleteVehicleByIdWhenItBelongsToAnotherPartner() {
+        String foreignPartnerToken = createAndLoginAsRentingPartner();
+        Account foreignPartner = getCurrentAccount(foreignPartnerToken);
+        Location foreignPartnerLocation = createAndSaveLocation(foreignPartner);
+        Vehicle foreignPartnerVehicle = createVehicle(foreignPartnerLocation, foreignPartner);
+
+        String token = createAndLoginAsRentingPartner();
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+        .when()
+            .delete("/vehicles/{id}", foreignPartnerVehicle.getId())
+        .then()
+            .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Should fail to delete vehicle that do not exist")
+    void shouldFailToDeleteVehicleThatDoNotExist() {
+        String token = createAndLoginAsRentingPartner();
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+        .when()
+            .delete("/vehicles/{id}", 9999)
+        .then()
+            .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Should deactivate vehicle successfully")
+    void shouldDeactivateVehicleSuccessfully() {
+        String token = createAndLoginAsRentingPartner();
+        Account account = getCurrentAccount(token);
+        Location location = createAndSaveLocation(account);
+        Vehicle vehicle = createVehicle(location, account);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+        .when()
+            .patch("/vehicles/{id}/deactivate", vehicle.getId())
+        .then()
+            .statusCode(204);
+
+        Vehicle afterVehicle = vehicleRepository.findById(vehicle.getId()).orElseThrow();
+        assertFalse(afterVehicle.getIsActive());
+    }
+
+    @Test
+    @DisplayName("Should activate vehicle successfully")
+    void shouldActivateVehicleSuccessfully() {
+        String token = createAndLoginAsRentingPartner();
+        Account account = getCurrentAccount(token);
+        Location location = createAndSaveLocation(account);
+        Vehicle vehicle = createVehicle(location, account);
+        vehicle.setIsActive(false);
+        vehicleRepository.save(vehicle);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+        .when()
+            .patch("/vehicles/{id}/activate", vehicle.getId())
+        .then()
+            .statusCode(204);
+
+        Vehicle afterVehicle = vehicleRepository.findById(vehicle.getId()).orElseThrow();
+        assert(afterVehicle.getIsActive());
+    }
+
+    @Test
+    @DisplayName("Should return available vehicles when filters match")
+    void shouldReturnAvailableVehiclesWhenFiltersMatch() {
+        String token = createAndLoginAsCustomer();
+        Account account = getCurrentAccount(token);
+        Location location = createAndSaveLocation(account);
+        Vehicle vehicle = createVehicle(location, account);
+
+        LocalDate startDate = LocalDate.now().plusDays(5);
+        LocalDate endDate = startDate.plusDays(3);
+
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+            .queryParam("city", location.getCity())
+            .queryParam("startDate", startDate.toString())
+            .queryParam("endDate", endDate.toString())
+        .when()
+            .get("/vehicles/available")
+        .then()
+            .statusCode(200)
+            .body("", hasSize(1))
+            .body("id", hasItem(vehicle.getId().intValue()));
+    }
+
+    @Test
+    @DisplayName("Should not return vehicle already rented in the requested period")
+    void shouldNotReturnVehicleAlreadyRentedInTheRequestedPeriod() {
+        String token = createAndLoginAsCustomer();
+        Account account = getCurrentAccount(token);
+
+        String partnerToken = createAndLoginAsRentingPartner();
+        Account partnerAccount = getCurrentAccount(partnerToken);
+
+        Location location = createAndSaveLocation(partnerAccount);
+        Vehicle vehicle = createVehicle(location, partnerAccount);
+
+        LocalDate startDate = LocalDate.now().plusDays(5);
+        LocalDate endDate = startDate.plusDays(3);
+
+        String foreignCustomerToken = createAndLoginAsCustomer();
+        Account foreignCustomer = getCurrentAccount(foreignCustomerToken);
+        Booking booking = new Booking();
+        booking.setVehicle(vehicle);
+        booking.setRenter(foreignCustomer);
+        booking.setStartDate(startDate);
+        booking.setEndDate(endDate);
+        booking.setStatus(BookingStatus.PENDING);
+        booking.setTotalPriceCents(10000L);
+
+        bookingRepository.save(booking);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer "  + token)
+            .queryParam("city", location.getCity())
+            .queryParam("startDate", startDate.toString())
+            .queryParam("endDate", endDate.minusDays(1).toString())
+        .when()
+            .get("/vehicles/available")
+        .then()
+            .statusCode(200)
+            .body("", hasSize(0));
     }
 
 
